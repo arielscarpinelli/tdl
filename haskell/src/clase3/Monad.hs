f1e8a37b738f9634b511e77df735db0648a83512
Ariel Scarpinelli <ascarpinelli@spredfast.com>
2018-12-14 21:30:57 -0300

    Update Monad.hs

--- haskell/src/clase3/Monad.hs
+++ haskell/src/clase3/Monad.hs
@@ -1,3 +1,12 @@
 module Monad where
 
--- TODO
+imperative_ish = do
+  name <- getLine
+  return ("Hello " ++ name)
+
+functional =
+  fmap (\name -> "Hello " ++ name) getLine
+
+main = do
+  print "Please enter your name"
+  functional
