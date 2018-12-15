230eb32e8bc390d0b39dcb63911c8e139438ae97
Ariel Scarpinelli <ascarpinelli@spredfast.com>
2018-12-15 08:04:40 -0300

    Update Monad.hs

--- haskell/src/clase3/Monad.hs
+++ haskell/src/clase3/Monad.hs
@@ -1,12 +1,34 @@
 module Monad where
 
+import Control.Monad
+
+-- class Monad m where
+--   (>>=)  :: m a -> (  a -> m b) -> m b
+--   return ::   a                 -> m a
+--   fmap :: (a -> b) -> f a -> f b -- It is a functor!
+--   join :: Monad m => m (m a) -> m a
+
 imperative_ish = do
+  print "Please enter your name"
   name <- getLine
-  return ("Hello " ++ name)
+  print ("Hello " ++ name)
 
 functional =
-  fmap (\name -> "Hello " ++ name) getLine
+  print "Please enter your name" >>=
+  \_ -> getLine >>=
+  \name -> return ("Hello " ++ name) >>=
+  \greet -> print greet
 
-main = do
-  print "Please enter your name"
-  functional
+underneath =
+  join (fmap (\greet -> print greet)
+    (join (fmap (\name -> return ("Hello " ++ name))
+      (join (fmap (\_ -> getLine) (print "Please enter your name"))))))
+
+-- why "join"?
+-- f = fmap (\_ -> getLine) (print "Please enter your name")
+-- :t f
+-- f :: IO (IO String)
+-- :t join f
+-- join f :: IO String
+
+main = underneath
