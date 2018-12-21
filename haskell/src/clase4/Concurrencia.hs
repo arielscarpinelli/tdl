d7e737f7b898754b979edb3b1fe2bf9f38bd1a6c
Ariel Scarpinelli <ascarpinelli@spredfast.com>
2018-12-21 08:03:37 -0300

    Update Concurrencia.hs

--- haskell/src/clase4/Concurrencia.hs
+++ haskell/src/clase4/Concurrencia.hs
@@ -3,40 +3,42 @@ module Concurrencia where
 import Control.Concurrent
 import Control.Concurrent.Chan
 
-reduceChannel (ch:s) reducer acc = do
-    value <- readChan ch
-    reduceChannel s reducer (reducer value acc)
+reduceMVars (mvar:s) reducer acc = do
+    value <- takeMVar mvar
+    reduceMVars s reducer (reducer value acc)
 
-reduceChannel [] reducer acc = return (acc)
+reduceMVars [] reducer acc = return (acc)
 
-firstThread ch = do
+firstThread mv = do
     threadDelay 200000 -- 200 ms
-    writeChan ch 4
+    putMVar mv 4
     print "first done"
 
-secondThread chIn chOut = do
-    readValue <- readChan chIn
-    writeChan chOut (readValue + 1)
+secondThread mvIn mvOut = do
+    readValue <- takeMVar mvIn
+    threadDelay 200000 -- 200 ms
+    putMVar mvOut (readValue + 1)
     print "second done"
 
-thirdThread ch = do
+thirdThread mv = do
     threadDelay 100000 -- 100 ms
-    writeChan ch 2
+    putMVar mv 2
     print "third done"
 
-fourthThread chIns chOut = do
-    sum <- reduceChannel chIns (+) 0
-    writeChan chOut sum
+fourthThread mvsIn mvOut = do
+    sum <- reduceMVars mvsIn (+) 0
+    putMVar mvOut sum
     print "fourth done"
 
 main = do
-  ch1 <- newChan
-  ch2 <- newChan
-  ch3 <- newChan
-  ch4 <- newChan
-  forkIO (firstThread ch1)
-  forkIO (secondThread ch1 ch2)
-  forkIO (thirdThread ch3)
-  forkIO (fourthThread [ch2, ch3] ch4)
+  mv1 <- newEmptyMVar
+  mv2 <- newEmptyMVar
+  mv3 <- newEmptyMVar
+  mv4 <- newEmptyMVar
+  forkIO (firstThread mv1)
+  forkIO (secondThread mv1 mv2)
+  forkIO (thirdThread mv3)
+  forkIO (fourthThread [mv2, mv3] mv4)
   print "im here"
-  readChan ch4 >>= print
+  result <- takeMVar mv4
+  print result
