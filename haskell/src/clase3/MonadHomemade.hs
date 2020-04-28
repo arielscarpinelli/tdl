6d2b1e36842263f5d308a6f531681810d8ab844b
Ariel Scarpinelli <ascarpinelli@spredfast.com>
2020-04-28 12:22:22 -0300

    actualiza ejemplo de monad

--- haskell/src/clase3/MonadHomemade.hs
+++ haskell/src/clase3/MonadHomemade.hs
@@ -8,14 +8,14 @@ type MyIO t = OutsideWorld -> (t, OutsideWorld)
 myPrint s (inputs, outputs) = ((), (inputs, outputs ++ [s]))
 myRead (i:nputs, outputs) = (i, (nputs, outputs))
 
+bind :: MyIO a -> (a -> MyIO b) -> MyIO b
 bind action1 action2 = \outsideworld ->
   let (action1Result, updatedWorld) = action1 outsideworld
   in action2 action1Result updatedWorld
 
-run ioOp = ioOp (["ariel"], [])
-
-main =
-  print (run (
+program =
     myPrint "Please enter your name" `bind`
     \ignore -> myRead  `bind`
-    \name -> myPrint ("Hello " ++ name)))
+    \name -> myPrint ("Hello " ++ name)
+
+main = print (program (["ariel"], []))
